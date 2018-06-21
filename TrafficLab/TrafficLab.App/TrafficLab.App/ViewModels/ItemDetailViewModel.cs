be7ef31e32e2513f9ecc7f726aa8ecf81a908d85
Andrés Londoño <andreslon@outlook.com>
2018-06-20 23:06:32 -0500

    Initial project

---
+++ TrafficLab/TrafficLab.App/TrafficLab.App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TrafficLab.App.Models;
+
+namespace TrafficLab.App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
